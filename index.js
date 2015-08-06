13f33a9938f9825aa84bcd10fe6bec003b027842
Brian Woodward <brian.woodward@gmail.com>
2015-08-06 01:08:57 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * bar <https://github.com/beepbop/bar>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
